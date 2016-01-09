df5b97615abe4299c4f2063354ba029d27ddc23d
Derek Feichtinger <derek.feichtinger@psi.ch>
2016-01-09 21:27:59 +0100

    added README and minimal documentation

--- org-clock-convenience.el
+++ org-clock-convenience.el
@@ -1,7 +1,26 @@
 ;;; org-clock-convenience --- convenience functions for org time tracking
 
+;; Author: Derek Feichtinger <derek.feichtinger@psi.ch>
+;; Keywords: org
+;; Homepage: https://github.com/dfeich/org-clock-convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
-;; Convenience functions for easier time tracking
+;; Convenience functions for easier time tracking. Provides commands
+;; for changing timestamps directly from the agenda view.
 
 ;;; Code:
 (require 'org)
