41f3c520cbaf365e00162f1be80faeb2a58c00c3
Derek Feichtinger <derek.feichtinger@psi.ch>
2022-04-23 18:43:11 +0200

    implemented tests using the occ-with-tempagenda macro

--- test/test-org-clock-convenience.el
+++ test/test-org-clock-convenience.el
@@ -2,20 +2,42 @@
 (require 'org-clock-convenience)
 (require 'cl-lib)
 
+(defvar occ-testagenda1 "* TaskA
+  :LOGBOOK:
+  CLOCK: [2022-04-15 Fri 08:00]--[2022-04-15 Fri 09:00] =>  1:00
+  :END:
+
+* TaskB
+  :LOGBOOK:
+  CLOCK: [2022-04-15 Fri 10:00]--[2022-04-15 Fri 10:05] =>  0:05
+  :END:
+
+* TaskC
+  :LOGBOOK:
+  CLOCK: [2022-04-15 Fri 11:00]--[2022-04-15 Fri 12:00] =>  1:00
+  :END:
+
+")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helper Functions
+
 (defun occ-extract-re-vals (fields re re-fields)
   (let ((pos (point))
-        (line (buffer-substring (point)
+        (line (buffer-substring (progn (forward-line 0) (point))
                                 (progn (forward-line 1) (point))))
         val)
     (cl-loop
      for field in fields
      do (progn
           (goto-char pos)
+          (forward-line 0)
           (setq val 
                 (org-clock-convenience-get-re-field
                  field re re-fields
                  (format "Regexp does not match line:\n%s" line))))
      collect (list field val) into results
+     finally do (goto-char pos)
      finally return results)))
 
 (defun occ-extract-agdline-vals (fields)
@@ -48,48 +70,29 @@
                    org-clock-convenience-tr-re
                    org-clock-convenience-tr-fields))
 
-(defun occ-create-agenda(filename)
-  "Create a test agenda file"
-  (with-temp-buffer
-    (insert "* TaskA
-  :LOGBOOK:
-  CLOCK: [2022-04-15 Fri 08:00]--[2022-04-15 Fri 09:00] =>  1:00
-  :END:
+(defmacro occ-with-tempagenda (orgentries datestr &rest body)
+  "Create an angeda view from the ORGENTRIES string and execute BODY.
+The agenda view will be created for the date given in DATESTR.
+The agenda buffer will be current and point will be at point-min
+when executing BODY."
+  `(let* ((testfname (make-temp-file "occ-test-agenda-" nil ".org"))
+          (org-agenda-files (list testfname))
+          (org-agenda-start-with-log-mode t))
+     (with-temp-file testfname
+       (insert ,orgentries))
+     (org-agenda-list 1 ,datestr)
+     (with-current-buffer org-agenda-buffer
+       (goto-char (point-min))
+       ,@body)
+     (set-buffer (get-file-buffer testfname))
+     (save-buffer)
+     (kill-this-buffer)
+     (delete-file testfname)))
 
-* TaskB
-  :LOGBOOK:
-  CLOCK: [2022-04-15 Fri 10:00]--[2022-04-15 Fri 10:05] =>  0:05
-  :END:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Test definitions
 
-* TaskC
-  :LOGBOOK:
-  CLOCK: [2022-04-15 Fri 11:00]--[2022-04-15 Fri 12:00] =>  1:00
-  :END:
-
-")
-    (write-file filename)))
-
-;; Tests
-
-;; older agenda default format lines contained space-padded hours (a space
-;; after the dash and a single hour digit)
-(ert-deftest agenda-re1 ()
-  (let ((line "  tasks2020:   7:55- 9:20 Clocked:   (0:10) Email and Misc")
-        (res '((d1-hours " 7")
-               (d1-minutes "55")
-               (d2-hours " 9")
-               (d2-minutes "20"))))
-    (occ-test-agenda-regexp line res)))
-
-(ert-deftest agenda-re2 ()
-  (let ((line "  tasks2020:   7:55-9:20 Clocked:   (0:10) Email and Misc")
-        (res '((d1-hours " 7")
-               (d1-minutes "55")
-               (d2-hours "9")
-               (d2-minutes "20"))))
-    (occ-test-agenda-regexp line res)))
-
-(ert-deftest clockline-re1 ()
+(ert-deftest occ-test-clockline-re1 ()
   (let ((line "    CLOCK: [2016-01-15 Fri 15:25]--[2016-01-15 Fri 18:10] =>  2:45")
         (res '((d1-year "2016") (d1-month "01") (d1-day "15")
                (d1-hours "15") (d1-minutes "25")
@@ -97,62 +100,84 @@
                (d2-hours "18") (d2-minutes "10"))))
     (occ-test-clockline-regexp line res)))
 
-
-(defmacro occ-with-tempagenda (orgentries datestr body)
-  "docstring"
-  `(let* ((testfname (make-temp-file "occ-test-agenda-" nil ".org")))
-     (with-temp-file testfname
-       (insert ,orgentries))
-     (setq org-agenda-files (list testfname)
-           org-agenda-start-with-log-mode t)
-     (org-agenda-list 1 ,datestr)
-     (with-current-buffer org-agenda-buffer
-       ,body)
-     (set-buffer (get-file-buffer testfname))
-     (save-buffer)
-     (kill-this-buffer)
-     (delete-file testfname)))
-
-(defun occ-test-macro ()
+(ert-deftest occ-test-agenda-re1 ()
   (occ-with-tempagenda
-   "* TaskA
-  :LOGBOOK:
-  CLOCK: [2022-04-15 Fri 08:00]--[2022-04-15 Fri 09:00] =>  1:00
-  :END:
+   occ-testagenda1 "2022-04-15"
+   (search-forward "TaskA")
+   (should
+    (equal (occ-extract-agdline-vals '(d1-time d2-time))
+           '((d1-time " 8:00") (d2-time " 9:00"))))))
 
-* TaskB
-  :LOGBOOK:
-  CLOCK: [2022-04-15 Fri 10:00]--[2022-04-15 Fri 10:05] =>  0:05
-  :END:
+(ert-deftest occ-test-timestamp-change-hours ()
+  (occ-with-tempagenda
+   occ-testagenda1 "2022-04-15"
+   (search-forward "TaskB")
+   (forward-line 0)
+   (org-clock-convenience-goto-agenda-tr-field 'd2-hours)
+   (org-clock-convenience-timestamp-change 2)
+   (should
+    (equal (occ-extract-agdline-vals '(d1-time d2-time))
+           '((d1-time "10:00") (d2-time "12:05"))))
+   ;; also test whether the org source file was changed
+   (org-clock-convenience-goto-ts)
+   (should
+    (equal (occ-extract-clockline-vals '(d1-time d2-time))
+           '((d1-time " 10:00") (d2-time " 12:05"))))))
 
-* TaskC
-  :LOGBOOK:
-  CLOCK: [2022-04-15 Fri 11:00]--[2022-04-15 Fri 12:00] =>  1:00
-  :END:
+(ert-deftest occ-test-timestamp-change-minutes ()
+  (occ-with-tempagenda
+   occ-testagenda1 "2022-04-15"
+   (search-forward "TaskB")
+   (forward-line 0)
+   (org-clock-convenience-goto-agenda-tr-field 'd2-minutes)
+   (let ((org-time-stamp-rounding-minutes '(0 10)))
+     (org-clock-convenience-timestamp-change 1))
+   (should
+    (equal (occ-extract-agdline-vals '(d1-time d2-time))
+           '((d1-time "10:00") (d2-time "10:10"))))
+   ;; also test whether the org source file was changed
+   (org-clock-convenience-goto-ts)
+   (should
+    (equal (occ-extract-clockline-vals '(d1-time d2-time))
+           '((d1-time " 10:00") (d2-time " 10:10"))))))
 
-"
-   "2022-04-15"
-   (princ (format "testfile: %s\n%s\n" testfname (buffer-string)))))
+;; NOTE: There currently is a problem with UNDO
+;; (ert-deftest occ-test-timestamp-change-undo ()
+;;   (occ-with-tempagenda
+;;    occ-testagenda1 "2022-04-15"
+;;    (search-forward "TaskB")
+;;    (forward-line 0)
+;;    (org-clock-convenience-goto-agenda-tr-field 'd2-hours)
+;;    (org-clock-convenience-timestamp-change 2)
+;;    (undo)
+;;    ;; (save-excursion
+;;    ;;   (princ
+;;    ;;    (format "DEBUG: %s"  (buffer-substring (progn (forward-line 0) (point))
+;;    ;;                                           (progn (forward-line 1) (point))))))
+;;    (should
+;;     (equal (occ-extract-agdline-vals '(d1-time d2-time))
+;;            '((d1-time "10:00") (d2-time "12:05"))))
+;;    ;; also test whether the org source file was changed
+;;    (org-clock-convenience-goto-ts)
+;;    (should
+;;     (equal (occ-extract-clockline-vals '(d1-time d2-time))
+;;            '((d1-time " 10:00") (d2-time " 12:05"))))))
 
-(defun test-agenda ()
-  (let* ((testfname (make-temp-file "occ-test-agenda-" nil ".org")))
-    (setq org-agenda-files `(,testfname)
-          org-agenda-start-with-log-mode t)
-    (occ-create-agenda testfname)
-    (org-agenda-list 1 "2022-04-15")
-    (with-current-buffer org-agenda-buffer
-      (princ (format "testfile: %s\n%s\n" testfname (buffer-string)))
-      (goto-char (point-min))
-      (org-clock-convenience-forward-log-line)
-      (org-clock-convenience-forward-log-line)
-      (forward-line 0)
-      (princ (format "before: %s\n" (occ-extract-agdline-vals '(d1-time d2-time))))
-      (forward-line 0)
-      (org-clock-convenience-goto-agenda-tr-field 'd2-minutes)
-      (org-clock-convenience-timestamp-up)
-      (forward-line 0)
-      (princ (format "after: %s\n" (occ-extract-agdline-vals '(d1-time d2-time)))))
-    (set-buffer (get-file-buffer testfname))
-    (save-buffer)
-    (kill-this-buffer)
-    (delete-file testfname)))
+(ert-deftest occ-test-fill-gap-both ()
+  (occ-with-tempagenda
+   occ-testagenda1 "2022-04-15"
+   (search-forward "TaskB")
+   (org-clock-convenience-fill-gap-both)
+   (should
+    (equal (occ-extract-agdline-vals '(d1-time d2-time))
+           '((d1-time "09:00") (d2-time "11:00"))))
+   ;; also test whether the org source file was changed
+   (forward-line 0)
+   (org-clock-convenience-goto-agenda-tr-field 'd2-hours)
+   (org-clock-convenience-goto-ts)
+   ;; (save-excursion  (princ  (buffer-substring (progn (forward-line 0) (point))
+   ;;                                            (progn (forward-line 1) (point)))))
+   (should
+    (equal (occ-extract-clockline-vals '(d1-time d2-time))
+           '((d1-time " 09:00") (d2-time " 11:00"))))))
+
